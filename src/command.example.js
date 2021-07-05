468d738cf4e8e1930641cf7c18f1f17b56c1e5f5
DanericNetwork <42703027+DanericNetwork@users.noreply.github.com>
2021-07-05 23:26:04 +0200

    Created Command Example

---
+++ src/command.example.js
@@ -0,0 +1,16 @@
+module.exports.run = async (client, message, args) => {
+    // Command Code
+};
+
+module.exports.conf = {
+    name: '',
+    description: '',
+    category: '',
+
+    aliases: [''],
+    usage: '',
+    example: '',
+
+    ownerOnly: true/false,
+    guildOnly: true/false
+}
